d4ace0668d888f39f89e853f88984e7935db5dd5
gkpgaurav06 <gaurav.nitkkr16.com>
2019-01-16 10:32:02 +0530

    Task1Commit

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,13 @@
+package com.stackroute.domain;
+
+public class Movie {
+    private Actor actor;
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+}
